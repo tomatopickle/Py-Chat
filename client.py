5599020a72aa70844c4b739e07ddfa4a559a9c29
Abaan Shanid <abaanashanid@gmail.com>
2022-12-31 15:13:35 +0300

    Feat: Added comments

--- client.py
+++ client.py
@@ -7,46 +7,66 @@ import traceback
 import logging
 from pyfiglet import Figlet
 
+# Create a Figlet object with the 'larry3d' font
 f = Figlet(font='larry3d')
-print (f.renderText('Py Chat Client'))
 
+# Print an ASCII art banner
+print(f.renderText('Py Chat Client'))
+
+# Prompt the user to choose a nickname
 nickname = input("Choose your nickname: ")
+
+# Prompt the user to enter a room id
 room = input("Room Id: ")
 
-
+# Create a socket and connect to the server at the specified IP and port
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.1.50', 7976))
 
+# Create a list of colors that can be applied to messages
 colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
+# Randomly select a color from the list to apply to the user's messages
 color = colors[random.randrange(len(colors))]
 
 
 def receive():
+    # Continuously receive messages from the server
     while True:
         try:
+            # Deserialize the message received from the server
             message = pickle.loads(client.recv(1024))
+
+            # If the message is from the server, send a message back with the user's nickname and room id
             if message.get('type') == 'server':
                 client.send(pickle.dumps({'name': nickname, 'roomId': room}))
             else:
+                # If the message is from another client and is intended for the current room, print it to the console
                 if (message['roomId'] == room):
                     print(message['msg'])
         except Exception as e:
+            # If an error occurs, print a message, log the error, close the socket, and break out of the loop
             print("An error occured!")
             logging.error(traceback.format_exc())
-
             client.close()
             break
 
 
 def write():
+    # Continuously prompt the user for input and send it as a message to the server
     while True:
         message = {'msg': colored(nickname, color) +
                    ': ' + input(''), "roomId": room}
         client.send(pickle.dumps(message))
 
-
+# Create a thread to run the receive function
 receive_thread = threading.Thread(target=receive)
+
+# Start the receive thread
 receive_thread.start()
+
+# Create a thread to run the write function
 write_thread = threading.Thread(target=write)
+
+# Start the write thread
 write_thread.start()
--- server.py
+++ server.py
@@ -4,59 +4,78 @@ import pickle
 from termcolor import colored
 from pyfiglet import Figlet
 
-host = '192.168.1.50'  # LocalHost
-port = 7976  # Choosing unreserved port
+# Choosing an unreserved port
+port = 7976
 
+# Create a socket and bind it to the host and port
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((socket.gethostname(), port))  # binding host and port to socket
+server.bind((socket.gethostname(), port))
 server.listen()
 
-print('Listening at port 7976, hostname:', socket.gethostname())
 
+# Create lists to store connected clients and their data
 clients = []
 clientData = []
 
+# Print an ASCII art banner with the text "Py Chat Server Running"
 f = Figlet(font='larry3d')
 print(f.renderText('Py Chat Server Running'))
 
+# Print the hostname and port that the server is listening on
+print('Listening at port 7976, hostname:', socket.gethostname())
 
 def broadcast(message):
+    """Broadcast a message to all connected clients"""
     for client in clients:
         client.send(message)
 
 
 def handle(client):
+    """Receive and broadcast messages from a single client"""
     while True:
         try:
+            # Receive a message from the client
             message = client.recv(1024)
+            # Broadcast the message to all other clients
             broadcast(message)
         except:
+            # If an error occurs, remove the client and close the connection
             index = clients.index(client)
             clients.remove(client)
             client.close()
+            # Remove the client's data from the list
             data = clientData[index]
             nickname = data['name']
+            # Broadcast a message to all other clients that the client has left
             broadcast(pickle.dumps({'msg': colored(nickname + ' left', 'red'),
-                      "roomId": data['roomId']}))
-            print(nickname+' left!')
+                                   "roomId": data['roomId']}))
+            print(nickname + ' left!')
             clientData.remove(data)
             break
 
 
 def receive():
+    """Accept new client connections and start a thread for each one"""
     while True:
+        # Accept a new connection
         client, address = server.accept()
         print("Connected with {}".format(str(address)))
+        # Send a message to the client asking for their data
         client.send(pickle.dumps({'type': 'server', 'q': 'data'}))
+        # Receive the client's data
         msg = client.recv(1024)
         data = pickle.loads(msg)
+        # Add the client's data to the list
         clientData.append(data)
         clients.append(client)
         print("Nickname is {}".format(data['name']))
+        # Broadcast a message to all other clients that the new client has joined
         broadcast(pickle.dumps(
             {"msg": colored("{} joined!".format(data['name']), 'green'), "roomId": data['roomId']}))
+        # Send a message to the new client with the room id
         client.send(pickle.dumps(
             {"msg": f"Connected to Sever! Room ID:{data['roomId']}", "roomId": data['roomId']}))
+        # Start a new thread to handle the new client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
